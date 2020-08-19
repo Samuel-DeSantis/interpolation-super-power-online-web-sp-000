f7eeb7ebd74d4e282e45a9c2eec211a9f6a6f336
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-08-19 03:45:45 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,7 @@
 def display_rainbow(array)
-  items = []
-  array.each { |i| items.push("#{i[0].upcase}: #{i},")}
-  items.pop()
-  puts items
+  items = ""
+  array.each { |i| items += "#{i[0].upcase}: #{i}, "}
+  items.chop!
+  items.chop!
+  print items
 end
