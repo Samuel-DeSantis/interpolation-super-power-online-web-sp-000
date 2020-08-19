e85d1e5c34818e9ca5415c69ac4c3e1cfe4760d9
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-08-19 03:45:17 +0000

    Automatically backed up by Learn

--- ex.rb
+++ ex.rb
@@ -2,7 +2,7 @@ colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
 def display_rainbow(array)
   items = ""
-  array.each { |i| items += "#{i[0].upcase}: #{i},"}
+  array.each { |i| items += "#{i[0].upcase}: #{i}, "}
   items.chop!
   print items
 end
