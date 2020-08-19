fe38ac37c546c39efb5f993db68ea70e5c4c356f
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-08-19 03:41:13 +0000

    Automatically backed up by Learn

--- ex.rb
+++ ex.rb
@@ -1,6 +1,10 @@
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
 def display_rainbow(array)
   items = []
   array.each { |i| items.push("#{i[0].upcase}: #{i},")}
   items.pop()
-  return items
+  puts items
 end
+
+display_rainbow(colors)
